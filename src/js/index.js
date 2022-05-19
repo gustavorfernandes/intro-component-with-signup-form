d0c04dabbac5096ff057a298fadef579a86af4b7
Gustavo <gustavo.ribeiro.fernandes1@gmail.com>
2022-05-19 15:46:21 -0300

    Improving project design

---
+++ src/js/index.js
@@ -0,0 +1,58 @@
+export function validate(input) {
+  const inputType = input.dataset.type;
+  const inputfield = input.parentElement.querySelector('.input');
+  const inputIcon = input.parentElement.querySelector('.input__icon');
+  const inputSpam = input.parentElement.querySelector('.input__span');
+
+  if (input.validity.valid) {
+    inputfield.classList.remove('input-error');
+    inputIcon.classList.remove('input-error__icon');
+    inputSpam.classList.remove('input-error__span');
+    inputSpam.innerHTML = '';
+  } else {
+    inputfield.classList.add('input-error');
+    inputIcon.classList.add('input-error__icon');
+    inputSpam.classList.add('input-error__span');
+    inputSpam.innerHTML = showErrorMessage(inputType, input);
+  }
+}
+
+const errorTypes = [
+  'valueMissing',
+  'typeMismatch'
+]
+
+const errorMessages = {
+  firstName: {
+    valueMissing: 'First Name cannot be empty'
+  },
+  lastName: {
+    valueMissing: 'Last Name cannot be empty'
+  },
+  email: {
+    valueMissing: 'Email cannot be empty',
+    typeMismatch: 'Looks like this is not an email'
+  },
+  password: {
+    valueMissing: 'Password cannot be empty'
+  }
+}
+
+function showErrorMessage(inputType, input) {
+  let message = '';
+
+  errorTypes.forEach(error => {
+    if (input.validity[error]) {
+      message = errorMessages[inputType][error];
+    }
+
+    if (message === 'Looks like this is not an email') {
+      const inputfield = input.parentElement.querySelector('.input');
+      inputfield.value = '';
+      inputfield.classList.add('placeholder-red');
+      inputfield.placeholder = 'email@example.com';
+    }
+  })
+
+  return message;
+}
